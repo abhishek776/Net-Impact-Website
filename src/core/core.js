cfc4aff3cb2a83d76f8ffa2536f0f481986fc335
Abhishek Fatehpuria <abhishekfatehpuria776@gmail.com>
2015-08-13 23:36:31 -0700

    fixed click handler

--- src/core/core.js
+++ src/core/core.js
@@ -2,7 +2,9 @@ $(document).ready(function() {
 
     $('#off-canvas-click').click(function(e) {
         if($('#offcanvas-1').attr('aria-hidden') === 'false') {
-            $('#offcanvas-1').click();
+            UIkit.offcanvas.hide([force = false]);
+        } else {
+            UIkit.offcanvas.show('#offcanvas-1');
         }
     });
 
