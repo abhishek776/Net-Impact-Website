432c39430ab9ba7b05cdfe8c3c8d499f22bf0860
Abhishek Fatehpuria <abhishekfatehpuria776@gmail.com>
2015-08-13 23:34:49 -0700

    repaird click handler

---
+++ src/core/core.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+
+    $('#off-canvas-click').click(function(e) {
+        if($('#offcanvas-1').attr('aria-hidden') === 'false') {
+            $('#offcanvas-1').click();
+        }
+    });
+
+
+})
